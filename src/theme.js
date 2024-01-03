b22437bc00acfe3e6123c0cebd2b370b03266c81
duongtranngoc <duongtn0306@gmail.com>
2024-01-03 21:41:12 +0700

    add constant style in theme.js and divide the website layout

--- src/theme.js
+++ src/theme.js
@@ -3,6 +3,10 @@ import { teal, deepOrange, cyan, orange } from '@mui/material/colors'
 
 // Create a theme instance.
 const theme = extendTheme({
+  trelloCustom: {
+    appBarHeight: '48px',
+    boardBarHeight: '56px'
+  },
   colorSchemes: {
     light: {
       palette: {
